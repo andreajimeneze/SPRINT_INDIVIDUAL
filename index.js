7cb776c51e19727b0d9ba006d107d803751b9a86
Andrea Jota <andreacjimenez@gmail.com>
2023-03-29 08:07:58 -0300

    algo de avance

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ import { join } from "path";
 import fs from "fs";
 import { fileURLToPath } from 'url';
 import hbs from "hbs";
-import path from 'path';
-import validator from "express-validator";
-import { capFirstMay, formatCL } from "./public/helpers/helpers.js";
 import router from './routes/routes.js';
-import morgan from "morgan";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 const productos= JSON.parse(fs.readFileSync("./data/data.json", "utf8"));
-
+const secreto = "secreto";
 export default productos;
 const __dirname = fileURLToPath(import.meta.url);
+
 const app = express()
 
 // Settings
@@ -20,19 +19,19 @@ app.set("view engine", "hbs")
 
 // Middlewares
 hbs.registerPartials(join(app.get("views"), "partials"));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(secreto));
+app.use(session({
+  secret: "secreto",
+  resave: true,
+  saveUninitialized: false
+}))
+
 app.use(router)
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
-app.use(express.static(join(__dirname, "./node_modules/bootstrap/dist")))
-app.use(express.static(join(__dirname, "./node_modules/jquery/dist")))
-
-app.get('/jquery.min.js', function(req, res) {
-  res.set('Content-Type', 'text/javascript');
-  res.sendFile(__dirname + '/jquery.min.js');
-});
+app.use(express.static("helpers"))
 
 app.listen(3000, () => {
   console.log("En Puerto 3000")
 })
-
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import fs from "fs";
 import productos from "../index.js";
-import users from "../index.js";
+import connection from "../conect.js";
+import session from "express-session";
 const router = express.Router();
 
 router.use(express.json());
@@ -13,7 +14,6 @@ router.get("/", (req, res) => {
 
 router.get("/tienda", (req, res) => {
   let dataPdto = productos.productos.map(d => {
-    console.log(productos.productos)
     return {
       id: d.id,
       nombre: d.nombre,
@@ -33,24 +33,106 @@ router.get("/ingreso", (req, res) => {
   res.render("ingreso")
 })
 
+router.get('/', function(req, res) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/ingreso.hbs'));
+});
+
+router.post('/auth', function(request, response) {
+	let username = request.body.usuario;
+	let password = request.body.password;
+	if (username && password) {
+		
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			
+			if (error) throw error;
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.send('Acceso exitoso');
+			} else {
+				response.send('Clave y/o usuario incorrecto');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Por favor, ingrese usuario y clave');
+		response.end();
+	}
+});
+
+connection.query(`SELECT * from accounts`,function (error, results, fields) {
+  if(error)
+  throw error;
+  results.forEach(result => {
+  });
+
+});
+
 router.post("/ingreso", (req, res) => {
-    
-  let { nombres, apellidos, rut, direccion, telefono, email, usuario1 } = req.body;
-  let users = JSON.parse(fs.readFileSync("./data/users.json"));
-  users.push({ nombres, apellidos, rut, direccion, telefono, email, usuario1 })
-  let user = JSON.stringify(users);
+  connection.query(`INSERT INTO registro (nombres, apellidos, rut, direccion, telefono, email, usuario ) VALUES ("${req.body.nombres}", "${req.body.apellidos}", "${req.body.rut}", "${req.body.direccion}", "${req.body.telefono}", "${req.body.email}", "${req.body.usuario1}")`, function (error, results, fields) {
+    if(error)
+    throw error;
+      console.log(results);
+      res.redirect("/ingreso")
+  });
+});
 
-  fs.writeFileSync("./data/users.json", user);
+// router.get("/cart", (req, res) => {
+// res.render("cart");
+// })
 
-  let { usuario, correo } = req.body;
-  let regisUsers = JSON.parse(fs.readFileSync("./data/users.json"));
-  console.log(regisUsers[0].usuario)
-  if((regisUsers.usuario|| regisUsers.email) == req.body ) {
-    console.log("usuario correcto")
-  } else { console.log("usuario no existe")}
+// router.get('/tienda', (req, res) => {
+//   const productId = req.query.productId;
+//   const product = getProductById(productId);
 
-  res.send("formulario enviado con éxito");
-})
+//   if (product) {
+//     const cart = req.session.cart || {};
+//     const cartProduct = cart[productId];
 
+//     if (cartProduct) {
+//       // Si el producto ya está en el carrito, incrementa su cantidad
+//       let cantProd = parseInt(req.query.cantProd)
+//       cartProduct.cantProd += product.cantProd;
+//     } else {
+//       // Si el producto no está en el carrito, agrégalo con una cantidad inicial de 1
+//       cart[productId] = {
+//         id: productId,
+//         name: product.nombre,
+//         price: product.precio,
+//         quantity: cantProd
+//       };
+//     }
+
+//     req.session.cart = cart;
+//   }
+
+//   res.redirect('/cart');
+// });
+
+
+
+// router.get("/tienda", (req, res) => {
+//   let addPdto = req.query.btnAdd;
+//  console.log(addPdto)
+//   if (addPdto) {
+//     let prod = req.query.pdtoId;
+//     let cart = req.session.cart || {};
+//     if (prod) {
+//       if (cart[prod]) {
+//         cart[prod].cantProd++;
+//       } else {
+//         cart[prod] = {
+//           id: prod,
+//           nombre: prod.nombre,
+//           precio: prod.precio,
+//           cantProd: prod.cantidad
+//         };
+//       }
+//       req.session.cart = cart;
+//     }
+//   }
+//   res.redirect("/cart");
+// });
 
 export default router;
