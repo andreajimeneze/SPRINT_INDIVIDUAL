d7d4386a343a7c4eb0f6dbdf93c2027acedc06e1
Andrea Jota <andreacjimenez@gmail.com>
2023-04-13 20:56:55 -0400

    Desarrollo de mantenedor agregar y eliminar producto

--- routes/routes.js
+++ routes/routes.js
@@ -5,16 +5,18 @@ import session from "express-session";
 import { Producto } from "../utils/Class/Producto.js";
 import { Usuario } from "../utils/Class/Usuario.js";
 import { Canasta } from "../utils/Class/Canasta.js";
+import methodOverride from "method-override";
 const router = express.Router();
 const cart = new Canasta();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+router.use(methodOverride("_method"));
 
 router.get("/", (req, res) => {
   res.render("index")
 })
-
+// CARGAR TIENDA CON PRODUCTOS
 router.get("/tienda", async (req, res) => {
   const pdto = new Producto()
   const productos = await pdto.getProducts()
@@ -38,6 +40,25 @@ router.get('/cart', (req, res) => {
   res.render("cart");
 })
 
+// ACCEDER A SESIÓN CON USUARIO Y PASSWORD
+// router.post('/auth', async (req, res) => {
+//   const { usuario, password } = req.body;
+//   if (!usuario || !password) {
+//     return res.send('Debe completar los campos requeridos');
+//   }
+//   const userN = new Usuario();
+//   const resultado = await userN.getUsuario(usuario, password);
+//   if (!resultado.success) {
+//     return res.send(resultado.message);
+//   }
+//   req.session.user = usuario;
+//   if (usuario === 'root') {
+//     return res.redirect('/adm');
+//   } else {
+//     return res.redirect('/tienda');
+//   }
+// });
+
 router.post('/auth', async (req, res) => {
   let username = req.body.usuario;
   let password = req.body.password;
@@ -49,14 +70,15 @@ router.post('/auth', async (req, res) => {
   if (usuario.success) {
     req.session.loggedin = true;
     req.session.username = usuario.usuario;
-    res.redirect('/cart');
-  } else {
-    res.redirect('/ingreso')
-  }
-}
+    res.redirect('/cart'); }
+    else if(username == "root" && password == "1234"){
+      res.redirect("/sesionadm")
+    } else { res.redirect('/ingreso')
+    console.log("contraseña no es correcta")}
+  } 
 );
 
-
+// REGISTRAR NUEVO USUARIO
 router.post("/ingreso", (req, res) => {
   const { nombres, apellidos, rut, direccion, telefono, email, usuario1, pass } = req.body;
 
@@ -71,10 +93,10 @@ router.get("/cart", (req, res) => {
   res.render("cart", { products: cart.items, total: cart.total });
 });
 
-
+// AGREGAR PRODUCTOS A LA CANASTA
 router.post("/cart", async (req, res) => {
   const pdtoId = parseInt(req.body.btnAdd);
-  
+
   const producto = new Producto()
   const prod = await producto.getProductById(pdtoId);
   await cart.addPdto(prod, 1);
@@ -83,16 +105,77 @@ router.post("/cart", async (req, res) => {
   res.render("cart", { products: cart.items, total: cart.total, canasta: canasta });
 });
 
+// VACIAR CANASTA
 router.delete("/cart", (req, res) => {
   const vaciar = parseInt(req.body.vaciar);
   cart.vaciarCarro()
   res.render("cart");
 })
 
+// ELIMINAR UN PRODUCTO DE LA CANASTA
 router.delete("/cart", (req, res) => {
   const trash = parseInt(req.body.trash);
   cart.deletePdto(trash)
   res.render("cart");
 })
 
+/*MANTENEDOR -----------------------------------------------------------------*/
+
+// AGREGAR NUEVO PRODUCTO DESDE MANTENEDOR
+router.post("/adm", async (req, res) => {
+  const { namePdto, prizePdto, imgPdto, cantPdto } = req.body;
+  let pdto = new Producto()
+  await pdto.addPdto(namePdto, parseInt(prizePdto), imgPdto, parseInt(cantPdto))
+
+  res.redirect("tienda");
+})
+
+
+// OBTENER PRODUCTOS PARA MANTENEDOR ELIMINAR
+router.get("/delete", async (req, res) => {
+  const pdto = new Producto()
+  const prod = await pdto.getProducts()
+
+  res.render("admin", { prod })
+})
+
+// ELIMINAR PRODUCTO DESDE MANTENEDOR
+router.delete("/adm/:id", async (req, res) => {
+  const pdtoEliminar = parseInt(req.params.id);
+  let pdto = new Producto()
+  await pdto.deletePdto(pdtoEliminar)
+  res.redirect("../tienda")
+})
+
+// MODIFICAR PRODUCTO DESDE MANTENEDOR
+router.put("/adm/:id", async (req, res) => {
+
+  let pdto = new Producto()
+  await pdto.modifPdto()
+  res.redirect("../tienda");
+})
+
+// ACCEDER A MODIFICARPDTO.HBS
+router.get("/modpdto", (req, res) => {
+  res.render("modificarPdto")
+} )
+
+// CARGAR PRODUCTOS EN MANTENEDOR
+router.get("/modpdto/form", async (req, res) => {
+  const pdto = new Producto()
+  const prod = await pdto.getProducts()
+
+  res.render("modificarPdto", { prod })
+})
+
+// SESIÓN ADMIN --- SE INGRESA CON USERNAME Y PASSWORD
+router.get("/sesionadm", (req, res) => {
+  res.render("sesionAdm")
+})
+
+router.get("/add", (req, res) => {
+res.render("addPdto")
+})
+
+router.get("")
 export default router;
--- utils/Class/Producto.js
+++ utils/Class/Producto.js
@@ -1,4 +1,5 @@
 import pool from "../../conect.js";
+// import { Canasta } from "../Class/Canasta.js";
 
 export class Producto {
     constructor(id, nombre, precio, foto, existencia) {
@@ -15,7 +16,7 @@ export class Producto {
         try {
             const consulta = await pool.query('SELECT * FROM producto');
 
-            consulta.rows.map(rows => {
+            consulta.rows.forEach(rows => {
                 dataPdto.push({
                     id: rows.id,
                     nombre: rows.nombre,
@@ -24,7 +25,7 @@ export class Producto {
                     existencia: rows.existencia
                 });
             })
-            
+
             return dataPdto;
         } catch (e) {
             throw e;
@@ -32,25 +33,80 @@ export class Producto {
     }
 
     async getProductById(id) {
-       
+
         try {
             const consulta = await pool.query('SELECT * FROM producto WHERE id = $1', [id]);
-            
+
             const item = consulta.rows[0]
-            
-            if(!item) { 
-                throw error(`Producto ${ id } no existe`)
+
+            if (!item) {
+                throw error(`Producto ${id} no existe`)
             }
-            return new Producto(                
-                    item.id,
-                    item.nombre,
-                    item.precio,
-                    item.imagen,
-                    item.existencia
-                );
+            return new Producto(
+                item.id,
+                item.nombre,
+                item.precio,
+                item.imagen,
+                item.existencia
+            );
         } catch (e) {
             throw e;
         }
     }
-    
+
+    async getProductByName(name) {
+
+        try {
+            const consulta = await pool.query('SELECT * FROM producto WHERE nombre = $1', [name]);
+
+            const item = consulta.rows[0]
+
+            if (!item) {
+                throw error(`Producto ${name} no existe`)
+            }
+            return new Producto(
+                item.id,
+                item.nombre,
+                item.precio,
+                item.imagen,
+                item.existencia
+            );
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async addPdto(nombre, precio, img, exist) {
+        const pdto = new Producto()
+        const bdpdtos = await pdto.getProducts()
+        let pdtoExist = bdpdtos.find((e) => e.nombre == nombre)
+
+        if (!pdtoExist) {
+            const consulta = await pool.query("INSERT INTO producto (nombre, precio, imagen, existencia) VALUES ($1, $2, $3, $4)", [nombre, precio, img, exist]);
+            return consulta.rows[0]
+        } else {
+            console.log("Producto ya existe en la Base de Datos")
+            return null
+        }
+    }
+
+    async deletePdto(id) {
+        try {
+            const consulta = await pool.query("DELETE from producto WHERE id = $1 RETURNING *", [id])
+            console.log(consulta.rows[0])
+
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async modifPdto(nombre, precio, imagen, existencia, id) {
+        try {
+            const cons = await pool.query("UPDATE producto SET nombre = $1 precio = $2 imagen = $3 existencia =$4 WHERE id = $5 RETURNING *", [nombre, precio, imagen, existencia, id])
+            console.table(cons.rows[0]);
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
 }
